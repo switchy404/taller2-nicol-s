f0226b30887cf5b915ba2624b63f9c0284f12f12
Nicolás Yépez Obando <n.yepez@uniandes.edu.co>
2024-08-19 20:50:42 -0500

    aysemeolvido

--- git_prueba.py
+++ git_prueba.py
@@ -6,3 +6,6 @@ print(f"Mi primer Githubsazo.")
 print(f"Te invito a mi segunda pandemia Monkipox <3")
 print("Trabajo de Juan Quintero (DeQu) y Nico Yepez (Switchy)")
 aysemeolvido=2
+
+aysemeolvido=aysemeolvido*2
+print(f"aysemeolvido {aysemeolvido}.")
