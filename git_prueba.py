e85ac2fca25d52b39440cca7e613818d0d19a925
TheOneTrueDeQu <j.quinterob@uniandes.edu.co>
2024-08-19 20:18:11 -0500

    commitaignorar

--- git_prueba.py
+++ git_prueba.py
@@ -5,3 +5,4 @@ print(f"Mi primera rama.")
 print(f"Mi primer Githubsazo.")
 print(f"Te invito a mi segunda pandemia Monkipox <3")
 print("Trabajo de Juan Quintero (DeQu) y Nico Yepez (Switchy)")
+aysemeolvido=2
